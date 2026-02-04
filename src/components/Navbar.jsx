862b52cef102edbb2f3c2a0773dcae102fc1fdfb
Lucas Hidalgo <lucashidalgo703@gmail.com>
2026-02-04 13:07:25 -0300

    Layout Fix: Header height 130px and Logo v5 (110px)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,18 @@ const Navbar = () => {
     <header className="navbar">
       <div className="container navbar-content">
         <Link to="/" className="logo">
-          <img src="/logo-header-v4.png" alt="AutoSporting" className="navbar-logo-img" />
+          <img src="/logo-header-v5.png" alt="AutoSporting" className="navbar-logo-img" />
         </Link>
+        ...
+        .navbar-logo-img {
+          height: 65px;
+        width: auto;
+        object-fit: contain;
+        }
+
+        @media (min-width: 768px) {
+            .navbar - logo - img {height: 110px; }
+        }
 
         {/* Desktop Menu */}
         <nav className="desktop-nav">
